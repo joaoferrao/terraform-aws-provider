6c511e134d994262aff62765b6edb756edf7e300
Datenn Worker <info@datenn.io>
2019-03-03 15:49:35 +0100

    example go-git commit

--- s3.tf
+++ s3.tf
@@ -106,3 +106,57 @@ module "my-server_encrypted_bucket" {
   kms_key_tags                       = "${var.bucket_my-server_kms_key_tags}"
   kms_key_description                = "${var.bucket_my-server_kms_key_description}"
 }
+################################################################################
+# S3 Encryted Bucket "DATENN_VAR"
+################################################################################
+
+module "my-bucket_encrypted_bucket" {
+  source                             = "./modules/terraform-s3-encrypted:v0.0.6"
+
+  region                             = "${var.bucket_my-bucket_region}"
+  s3_bucket_name                     = "my-bucket"
+  s3_bucket_acl                      = "private"
+  allow_terraform_destroy            = false
+  deletion_window_in_days            = "${var.bucket_my-bucket_deletion_window_in_days}"
+  kms_key_alias                      = "my-bucket-kms-key"
+  versioning_enabled                 = "${var.bucket_my-bucket_versioning_enabled}"
+  transition_lifecycle_rule_enabled  = "${var.bucket_my-bucket_transition_lifecycle_rule_enabled}"
+  transition_lifecycle_rule_prefix   = "${var.bucket_my-bucket_transition_lifecycle_rule_prefix}"
+  expiration_lifecycle_rule_enabled  = "${var.bucket_my-bucket_expiration_lifecycle_rule_enabled}"
+  expiration_lifecycle_rule_prefix   = "${var.bucket_my-bucket_expiration_lifecycle_rule_prefix}"
+  noncurrent_version_transition_days = "${var.bucket_my-bucket_noncurrent_version_transition_days}"
+  standard_transition_days           = "${var.bucket_my-bucket_standard_transition_days}"
+  glacier_transition_days            = "${var.bucket_my-bucket_glacier_transition_days}"
+  expiration_days                    = "${var.bucket_my-bucket_expiration_days}"
+  noncurrent_version_expiration_days = "${var.bucket_my-bucket_noncurrent_version_expiration_days}"
+  bucket_tags                        = "${var.bucket_my-bucket_tags}"
+  kms_key_tags                       = "${var.bucket_my-bucket_kms_key_tags}"
+  kms_key_description                = "${var.bucket_my-bucket_kms_key_description}"
+}
+################################################################################
+# S3 Encryted Bucket "DATENN_VAR"
+################################################################################
+
+module "my-server_encrypted_bucket" {
+  source                             = "./modules/terraform-s3-encrypted:v0.0.7"
+
+  region                             = "${var.bucket_my-server_region}"
+  s3_bucket_name                     = "my-server"
+  s3_bucket_acl                      = "private"
+  allow_terraform_destroy            = false
+  deletion_window_in_days            = "${var.bucket_my-server_deletion_window_in_days}"
+  kms_key_alias                      = "my-server-kms-key"
+  versioning_enabled                 = "${var.bucket_my-server_versioning_enabled}"
+  transition_lifecycle_rule_enabled  = "${var.bucket_my-server_transition_lifecycle_rule_enabled}"
+  transition_lifecycle_rule_prefix   = "${var.bucket_my-server_transition_lifecycle_rule_prefix}"
+  expiration_lifecycle_rule_enabled  = "${var.bucket_my-server_expiration_lifecycle_rule_enabled}"
+  expiration_lifecycle_rule_prefix   = "${var.bucket_my-server_expiration_lifecycle_rule_prefix}"
+  noncurrent_version_transition_days = "${var.bucket_my-server_noncurrent_version_transition_days}"
+  standard_transition_days           = "${var.bucket_my-server_standard_transition_days}"
+  glacier_transition_days            = "${var.bucket_my-server_glacier_transition_days}"
+  expiration_days                    = "${var.bucket_my-server_expiration_days}"
+  noncurrent_version_expiration_days = "${var.bucket_my-server_noncurrent_version_expiration_days}"
+  bucket_tags                        = "${var.bucket_my-server_tags}"
+  kms_key_tags                       = "${var.bucket_my-server_kms_key_tags}"
+  kms_key_description                = "${var.bucket_my-server_kms_key_description}"
+}
--- variables.tf
+++ variables.tf
@@ -310,3 +310,155 @@ variable "bucket_my-server_kms_key_description" {
   description = "KMS key description"
   default     = "This key is used to encrypt bucket objects"
 }
+################################################################################
+# S3 Bucket "my-bucket" variables
+################################################################################
+variable "bucket_my-bucket_region" {
+  description = "AWS Region for S3 Bucket my-bucket"
+  default     = "eu-west-1"
+}
+
+variable "bucket_my-bucket_deletion_window_in_days" {
+  description = "Duration in days after which the key is deleted after destruction of the resource, must be between 7 and 30 days. Defaults to 10 days."
+  default     = 10
+}
+
+variable "bucket_my-bucket_versioning_enabled" {
+  default = false
+}
+
+variable "bucket_my-bucket_transition_lifecycle_rule_enabled" {
+  default = false
+}
+
+variable "bucket_my-bucket_expiration_lifecycle_rule_enabled" {
+  default = false
+}
+
+variable "bucket_my-bucket_expiration_lifecycle_rule_prefix" {
+  description = "(Optional) expiration lifecycle rule, only valid if enabled"
+  default     = ""
+}
+
+variable "bucket_my-bucket_transition_lifecycle_rule_prefix" {
+  description = "(Optional) transition lifecycle rule, only valid if enabled"
+  default     = ""
+}
+
+variable "bucket_my-bucket_noncurrent_version_transition_days" {
+  description = "(Optional) Specifies when noncurrent object versions transitions"
+  default     = "30"
+}
+
+variable "bucket_my-bucket_standard_transition_days" {
+  description = "Number of days to persist in the standard storage tier before moving to the infrequent access tier"
+  default     = "30"
+}
+
+variable "bucket_my-bucket_glacier_transition_days" {
+  description = "Number of days after which to move the data to the glacier storage tier"
+  default     = "60"
+}
+
+variable "bucket_my-bucket_expiration_days" {
+  description = "Number of days after which to expunge the objects"
+  default     = "90"
+}
+
+variable "bucket_my-bucket_noncurrent_version_expiration_days" {
+  description = "(Optional) Specifies when noncurrent object versions expire."
+  default     = "90"
+}
+
+variable "bucket_my-bucket_tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('Environment','prd', 'Project', 'geo-analytics')"
+}
+
+variable "bucket_my-bucket_kms_key_tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('Environment','prd', 'Project', 'geo-analytics')"
+}
+
+variable "bucket_my-bucket_kms_key_description" {
+  description = "KMS key description"
+  default     = "This key is used to encrypt bucket objects"
+}
+################################################################################
+# S3 Bucket "my-server" variables
+################################################################################
+variable "bucket_my-server_region" {
+  description = "AWS Region for S3 Bucket my-server"
+  default     = "eu-west-1"
+}
+
+variable "bucket_my-server_deletion_window_in_days" {
+  description = "Duration in days after which the key is deleted after destruction of the resource, must be between 7 and 30 days. Defaults to 10 days."
+  default     = 10
+}
+
+variable "bucket_my-server_versioning_enabled" {
+  default = false
+}
+
+variable "bucket_my-server_transition_lifecycle_rule_enabled" {
+  default = false
+}
+
+variable "bucket_my-server_expiration_lifecycle_rule_enabled" {
+  default = false
+}
+
+variable "bucket_my-server_expiration_lifecycle_rule_prefix" {
+  description = "(Optional) expiration lifecycle rule, only valid if enabled"
+  default     = ""
+}
+
+variable "bucket_my-server_transition_lifecycle_rule_prefix" {
+  description = "(Optional) transition lifecycle rule, only valid if enabled"
+  default     = ""
+}
+
+variable "bucket_my-server_noncurrent_version_transition_days" {
+  description = "(Optional) Specifies when noncurrent object versions transitions"
+  default     = "30"
+}
+
+variable "bucket_my-server_standard_transition_days" {
+  description = "Number of days to persist in the standard storage tier before moving to the infrequent access tier"
+  default     = "30"
+}
+
+variable "bucket_my-server_glacier_transition_days" {
+  description = "Number of days after which to move the data to the glacier storage tier"
+  default     = "60"
+}
+
+variable "bucket_my-server_expiration_days" {
+  description = "Number of days after which to expunge the objects"
+  default     = "90"
+}
+
+variable "bucket_my-server_noncurrent_version_expiration_days" {
+  description = "(Optional) Specifies when noncurrent object versions expire."
+  default     = "90"
+}
+
+variable "bucket_my-server_tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('Environment','prd', 'Project', 'geo-analytics')"
+}
+
+variable "bucket_my-server_kms_key_tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('Environment','prd', 'Project', 'geo-analytics')"
+}
+
+variable "bucket_my-server_kms_key_description" {
+  description = "KMS key description"
+  default     = "This key is used to encrypt bucket objects"
+}
