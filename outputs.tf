01317bb2057ec19972e1b32c90805a04f385d457
Datenn Worker <info@datenn.io>
2019-03-03 15:48:30 +0100

    example go-git commit

--- outputs.tf
+++ outputs.tf
@@ -36,3 +36,41 @@ output "bucket_my-server_encrypted_bucket_s3_id" {
 output "bucket_my-server_encrypted_bucket_kms_key_arn" {
   value = "${module.DATENN_VAR_encrypted_bucket.aws_kms_key_arn}"
 }
+################################################################################
+# S3 bucket DATENN_VAR outputs
+################################################################################
+
+output "bucket_my-bucket_encrypted_bucket_name" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_bucket}"
+}
+
+output "bucket_my-bucket_encrypted_bucket_s3_arn" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_arn}"
+}
+
+output "bucket_my-bucket_encrypted_bucket_s3_id" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_id}"
+}
+
+output "bucket_my-bucket_encrypted_bucket_kms_key_arn" {
+  value = "${module.DATENN_VAR_encrypted_bucket.aws_kms_key_arn}"
+}
+################################################################################
+# S3 bucket DATENN_VAR outputs
+################################################################################
+
+output "bucket_my-server_encrypted_bucket_name" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_bucket}"
+}
+
+output "bucket_my-server_encrypted_bucket_s3_arn" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_arn}"
+}
+
+output "bucket_my-server_encrypted_bucket_s3_id" {
+  value = "${module.DATENN_VAR_encrypted_bucket.s3_id}"
+}
+
+output "bucket_my-server_encrypted_bucket_kms_key_arn" {
+  value = "${module.DATENN_VAR_encrypted_bucket.aws_kms_key_arn}"
+}
